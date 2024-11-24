ce720f8479b5c74db7d86e9fdf0f98169300bbf7
sararchh <sara_rocha99@hotmail.com>
2024-11-24 13:05:51 -0300

    feat: added dockerfile

--- backend/src/schemas/environment.schema.ts
+++ backend/src/schemas/environment.schema.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   ENV: z.enum(["development", "tests", "production"]).default("development"),
-  PORT: z.coerce.number().default(3001),
+  PORT: z.coerce.number().default(8080),
   GOOGLE_API_KEY: z.string(),
 });
